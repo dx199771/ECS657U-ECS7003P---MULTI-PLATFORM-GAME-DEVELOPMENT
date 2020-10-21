3e4ef8121273fb773351a97036c56dc33de0dcbd
dx199771 <xdong14@sheffield.ac.uk>
2020-10-21 01:36:01 +0100

    Hero movement animation

---
+++ MULTI-PLATFORM GAME DEVELOPMENT/Assets/PlayerAnimation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    public string[] staticDirections = { "Static North", "Static West", "Static South", "Static East" };
+    public string[] runDirections = { "Run North", "Run West", "Run South", "Run East" };
+
+    int lastDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+
+
+    public void SetDirection(Vector2 _direction) {
+
+        string[] directionArray = null;
+        if (_direction.magnitude < 0.01)
+        {
+            directionArray = staticDirections;
+        }
+        else {
+            directionArray = runDirections;
+            lastDirection = DirectionToIndex(_direction);
+        }
+        anim.Play(directionArray[lastDirection]);
+    }
+    private int DirectionToIndex(Vector2 _direction)
+    {
+        Vector2 norDir = _direction.normalized;
+        float step = 360 / 4;
+        float offset = step / 2;
+        float angle = Vector2.SignedAngle(Vector2.up, norDir);
+        angle += offset;
+        if (angle < 0) {
+            angle += 360;
+        }
+        float stepCount = angle / step;
+        
+        return Mathf.FloorToInt(stepCount);
+
+
+    }
+}
---
+++ MULTI-PLATFORM GAME DEVELOPMENT/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 500;
+     
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horAxis = Input.GetAxis("Horizontal");
+        float verAxis = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(horAxis,verAxis);
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horAxis,verAxis);
+        Vector2 direction = new Vector2(horAxis, verAxis);
+        FindObjectOfType<PlayerAnimation>().SetDirection(direction);
+    }
+
+}
