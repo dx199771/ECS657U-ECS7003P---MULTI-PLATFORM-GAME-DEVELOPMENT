19023fe9f3f31f4163a8c3244cce002f1a50b9c2
dx199771 <xdong14@sheffield.ac.uk>
2020-10-22 03:48:08 +0100

    Timer

---
+++ MULTI-PLATFORM GAME DEVELOPMENT/Assets/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+    public float intialTime;
+    public float currentTime;
+    public Text timeDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = intialTime;
+        timeDisplay.text = intialTime.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        timeDisplay.text = ((int)currentTime).ToString()+"s";
+
+    }
+}
