25c4ada01b791ffee57161e27ae22cf5bc99d954
dx199771 <xdong14@sheffield.ac.uk>
2020-10-23 02:22:09 +0100

    Health bar without UI

---
+++ MULTI-PLATFORM GAME DEVELOPMENT/Assets/HealthPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPoint : MonoBehaviour
+{
+    public Image healthPointImage;
+    public Image healthPointEffect;
+    private float hurtSpeed = 0.003f;
+
+    private PlayerControl player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>;
+
+    }
+
+    void Update() {
+
+        healthPointImage.fillAmount = player.currentHp / player.maxHp;
+        if (healthPointEffect.fillAmount > healthPointImage.fillAmount)
+        {
+            healthPointEffecct.fillAmount -= hurtSpeed;
+
+        }
+        else {
+            healthPointEffect.fillAmount = healthPointImage.fillAmount;
+        }
+    }
+}
---
+++ MULTI-PLATFORM GAME DEVELOPMENT/Assets/PlayerControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float maxHp = 100.0f;
+    public float currentHp = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHp = maxHp;
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        currentHp -= 15.0f;
+    }
+}
