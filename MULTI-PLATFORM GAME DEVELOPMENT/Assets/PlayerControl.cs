036ffd534b21762b7120e37976492d7faf3efefc
dx199771 <xdong14@sheffield.ac.uk>
2020-10-31 16:46:06 +0000

    audio manager

--- MULTI-PLATFORM GAME DEVELOPMENT/Assets/PlayerControl.cs
+++ MULTI-PLATFORM GAME DEVELOPMENT/Assets/PlayerControl.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public AudioSource tickSource;
     public float maxHp = 100.0f;
     public float currentHp = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
         currentHp = maxHp;
+        tickSource = GetComponent<AudioSource>();
+
     }
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Pedestrain"))
+        {
             currentHp -= 15.0f;
+            tickSource.Play();
+        }
         if(currentHp <= 0)
             FindObjectOfType<GameManager>().EndGame();
 
---
+++ MULTI-PLATFORM GAME DEVELOPMENT/Assets/ShoppingList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShoppingList : MonoBehaviour
+{
+    private int imageNum = 0;
+    private List<string> ImageName;
+    private Image slot1;
+    private Image slot2;
+    private Image slot3;
+    private Image slot4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shoppingList.text = "Hi";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
