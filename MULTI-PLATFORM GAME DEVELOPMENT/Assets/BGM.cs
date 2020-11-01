0e665f41dfed5b5741a6a3dbc3c0b8256120cb8a
Matrix J <33464122+Matrix-J-H@users.noreply.github.com>
2020-11-01 23:07:17 +0000

    BGM added

---
+++ MULTI-PLATFORM GAME DEVELOPMENT/Assets/BGM.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    private static BGM instance = null;
+    private AudioSource audio;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        if (instance == this) return;
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+}
