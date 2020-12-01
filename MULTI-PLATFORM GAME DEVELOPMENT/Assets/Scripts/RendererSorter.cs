9d2892fb876b0866b9915a5e09c36f28fdf764a1
Matrix J <33464122+Matrix-J-H@users.noreply.github.com>
2020-12-01 12:26:42 +0000

    Renderer Sorter Added

---
+++ MULTI-PLATFORM GAME DEVELOPMENT/Assets/Scripts/RendererSorter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RendererSorter : MonoBehaviour
+{
+    [SerializeField]
+    private int sortingOrderBase = 5000;
+    [SerializeField]
+    private int offset = 0;
+    private Renderer myRenderer;
+
+    private void Awake()
+    {
+        myRenderer = gameObject.GetComponent<Renderer>();
+    }
+
+    private void LateUpdate()
+    {
+        myRenderer.sortingOrder = (int)(sortingOrderBase - transform.position.y - offset);
+    }
+}
