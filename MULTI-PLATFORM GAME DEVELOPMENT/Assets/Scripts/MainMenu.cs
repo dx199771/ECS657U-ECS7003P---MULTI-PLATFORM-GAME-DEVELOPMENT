b688e12daba133e5c23c1a4499b1156f46ccb581
Matrix J <33464122+Matrix-J-H@users.noreply.github.com>
2020-12-07 05:11:55 +0000

    MainMenu BGM

--- MULTI-PLATFORM GAME DEVELOPMENT/Assets/Scripts/MainMenu.cs
+++ MULTI-PLATFORM GAME DEVELOPMENT/Assets/Scripts/MainMenu.cs
@@ -10,7 +10,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame() {
         SceneManager.LoadScene("Cutscene");
-        GameHandler.Save();
+        PlayerPrefs.DeleteAll();
 
     }
     public void Countiue()
